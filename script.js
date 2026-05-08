e1ee9f18046cfb15b7b4222583303d3c3f53b397
Luka <leticiaandradesilva613@gmail.com>
2026-05-08 12:32:07 -0300

    adicionando botão subir ao topo

--- script.js
+++ script.js
@@ -26,3 +26,20 @@ function digitarDescricao() {
 
 digitar();
 digitarDescricao();
+
+const botao = document.getElementById("topo");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 200) {
+    botao.style.display = "block";
+  } else {
+    botao.style.display = "none";
+  }
+});
+
+botao.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
